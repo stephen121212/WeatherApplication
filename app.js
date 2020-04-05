e47eef592918f0ac22dc007c981f731d9ead281b
stephen121212 <minechart1@gmail.com>
2020-04-05 22:24:59 +0100

    Adding Error Handling

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const fs = require('fs')
 const request = require('postman-request');
 
 request('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6c1ac23921559b3a3fd8ebbd9ecb5646', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  loadWeatherForeCast(body)
-  getWeatherForeCast()
-});
+  if(error){
+      console.log('Error: Connecting to service', error); // Print the error if one occurred
+  } else if (response.statusCode === 200) {
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        loadWeatherForeCast(body)
+        getWeatherForeCast()
+    } else if (response.body.features.length === 0) {
+        console.log('Error: Body length is 0')
+    }
+     else {
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    }
+  });
 
 const printWeatherForecast = (weather) => {
     fileExists()
@@ -19,9 +28,7 @@ const fileExists = () => {
             const dataBuffer = fs.readFileSync('weather.json')
             const weatherObject = JSON.parse(dataBuffer)
             console.log(weatherObject.coord.lat + ' ' + weatherObject.coord.lon)
-        } else{
-            console.log('Error occured')
-        }
+        } 
     } catch(err){
         console.error(err)
     }
